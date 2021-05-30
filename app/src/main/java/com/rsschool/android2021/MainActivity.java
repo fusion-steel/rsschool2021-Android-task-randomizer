0e75d2cb644df418ce6ef9b834b5ac2439d2ab4b
fusion-steel <pavelgently@gmail.com>
2021-05-30 22:56:56 +0300

    Feat: implement members openFirstFragment, openSecondFragment using interfaces. Data transaction implemented using FragmentManager.

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -1,13 +1,14 @@
 package com.rsschool.android2021;
 
 import android.os.Bundle;
-
+import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LaunchFirstFragment, LaunchSecondFragment {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
         openFirstFragment(0);
     }
 
-    private void openFirstFragment(int previousNumber) {
+    public void openFirstFragment(int previousNumber) {
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment);
-        // TODO: invoke function which apply changes of the transaction
+        transaction.commit();
     }
 
-    private void openSecondFragment(int min, int max) {
-        // TODO: implement it
+    public void openSecondFragment(int min, int max) {
+        final Fragment secondFragment = SecondFragment.newInstance(min, max);
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, secondFragment);
+        transaction.commit();
     }
 }
